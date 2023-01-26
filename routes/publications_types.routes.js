2244c3bca5d19eaa0dd2b48c0978bed16db8ddf8
Angel Carrasco <acarrascocesa@gmail.com>
2023-01-26 13:55:35 -0400

    feat: documetation added

--- routes/publications_types.routes.js
+++ routes/publications_types.routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
+
 const {
   getPublicationsTypes,
   addPublicationType,
@@ -9,9 +10,96 @@ const {
   removePublicationType,
 } = require('../controllers/publications_types.controller')
 
-router.get('/', getPublicationsTypes)
+/**
+ * @swagger
+ * components: 
+ *  schemas:
+ *    PublicationsTypes:
+ *      type: object
+ *      properties: 
+ *        name: 
+ *          type: varchar
+ *          description: the publications types
+ *        description:  
+ *          type: varchar
+ *          description: the description of the publications types
+ *      required: false
+ *      example: 
+ *        name: Movies
+ *        descrption: This publication is about movies
+ */
+
+/**
+ * @swagger
+ * /publications-types
+ *  get:
+ *    summary: get all publications types
+ *    tags: [PublicationsTypes] 
+ *    responses:
+        '200':
+          description: successful operation
+          content:
+            application/json:
+              schema:
+                type: array
+                items:
+                  $ref: '#/components/schemas/PublicationsTypes'          
+            application/xml:
+              schema:
+                type: array
+                items:
+                  $ref: '#/components/schemas/PublicationsTypes'
+        '400':
+          description: Invalid publication type
+ *        
+ */
+router.get('/publications-types', getPublicationsTypes)
+
 router.post('/', addPublicationType)
+
+/**
+ * @swagger
+ * components: 
+ *  schemas:
+ *    PublicationsTypesByID:
+ *      type: object
+ *      properties: 
+ *        name: 
+ *          type: varchar
+ *          description: the publications types by id
+ *        description:  
+ *          type: varchar
+ *          description: the description of the publications types by id
+ *      required: false
+ *     
+ */
+
+/**
+ * @swagger
+ * /:publication_type_id
+ *  get:
+ *    summary: get publications by id
+ *    tags: [PublicationsTypesByID] 
+ *    responses:
+        '200':
+          description: successful operation
+          content:
+            application/json:
+              schema:
+                type: array
+                items:
+                  $ref: '#/components/schemas/PublicationsTypesByID'          
+            application/xml:
+              schema:
+                type: array
+                items:
+                  $ref: '#/components/schemas/PublicationsTypesByID'
+        '400':
+          description: Invalid publication type
+ *        
+ */
 router.get('/:publication_type_id', getPublicationType)
+
 router.put('/:id', updatePublicationType)
 router.delete('/:id', removePublicationType)
 
--- routes/state.routes.js
+++ routes/state.routes.js
@@ -9,7 +9,45 @@ const {
   removeState,
 } = require('../controllers/state.controller')
 
-router.get('/', getStates)
+/**
+ * @swagger
+ * components: 
+ *  schemas:
+ *    States:
+ *      type: object
+ *      properties: 
+ *        name: 
+ *          type: varchar
+ *          description: get the states
+ *      required: false
+ */
+
+/**
+ * @swagger
+ * /states
+ *  get:
+ *    summary: get all the states
+ *    tags: [States] 
+ *    responses:
+        '200':
+          description: successful operation
+          content:
+            application/json:
+              schema:
+                type: array
+                items:
+                  $ref: '#/components/schemas/States'          
+            application/xml:
+              schema:
+                type: array
+                items:
+                  $ref: '#/components/schemas/States'
+        '400':
+          description: Invalid state
+ *        
+ */
+router.get('/states', getStates)
+
 router.post('/', addState)
 router.get('/:id', getState)
 router.put('/:id', updateState)
